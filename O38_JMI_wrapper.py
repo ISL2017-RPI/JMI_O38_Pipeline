68877e576d2065808f991ab5f6a2c7e501cd4902
ISL2017-RPI <31634008+ISL2017-RPI@users.noreply.github.com>
2017-09-08 15:48:25 -0400

    Update O38_JMI_wrapper.py

--- O38_JMI_wrapper.py
+++ O38_JMI_wrapper.py
@@ -2,7 +2,7 @@ import sys
 import JMI_O38
 import numpy as np
 
-def JMI(data_file = 'trainData.csv', target_file = 'trainargets.csv', hm_feat = 12):
+def JMI(data_file = 'trainData.csv', target_file = 'trainTargets.csv', hm_feat = 12):
     my_JMI = JMI_O38.initialize()
     feat = my_JMI.JMI_primitive_O38(data_file, target_file, hm_feat)
     return feat
